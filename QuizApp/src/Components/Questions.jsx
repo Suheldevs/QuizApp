2d9a97263e4eeb1a4ffce562d655e5b474de3320
MOHDSUHEL_01 <mohdsuhail16289@gmail.com>
2024-11-06 15:48:49 +0530

    create a Quiz app

---
+++ QuizApp/src/Components/Questions.jsx
@@ -0,0 +1,28 @@
+ const questions = [
+    {
+      question: "What is the largest planet in our solar system?",
+      options: ["Earth", "Jupiter", "Mars", "Saturn"],
+      answer: "Jupiter",
+    },
+    {
+      question: "Who wrote the play 'Romeo and Juliet'?",
+      options: ["Shakespeare", "Leo Tolstoy", "Homer", "Charles Dickens"],
+      answer: "Shakespeare",
+    },
+    {
+      question: "What is the capital of Japan?",
+      options: ["Seoul", "Beijing", "Tokyo", "Bangkok"],
+      answer: "Tokyo",
+    },
+    {
+      question: "What is the boiling point of water?",
+      options: ["90°C", "100°C", "110°C", "120°C"],
+      answer: "100°C",
+    },
+    {
+      question: "Which element has the chemical symbol 'O'?",
+      options: ["Oxygen", "Gold", "Osmium", "Silver"],
+      answer: "Oxygen",
+    }
+  ];
+  export default questions
